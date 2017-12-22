43c299aea308e20efa76624452ecefee18a2289a
Christopher Lawrence <chrislawrence05@gmail.com>
2017-12-22 07:04:48 -0500

    #14 - Adds support for jQuery 1.9
    - Adds tests for jQuery 1.9
    - Updated html tests to use dev server

---
+++ test/parsers/inBrowserTests/jquery19Parser.test.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import JQueryParser from '../../../src/parsers/jqueryParser';
+import { setTimeout } from 'timers';
+
+describe('jQuery 1.9 parser', () => {
+    let allElements: NodeListOf<Element>;
+    before(() => {
+        allElements = document.getElementsByTagName('*');
+    });
+
+    it('parses the test file and returns the correct number of events found (2)', (done) => {
+        setTimeout(() => {
+            const parser = new JQueryParser();
+            const result = parser.parse(allElements);
+            expect(result.length).to.be.eq(2, 'parser did not return correct number of elementListeners');
+            done();
+        }, 500);
+    });
+
+    it('parses the test file and returns the correct number of events (2)', (done) => {
+        setTimeout(() => {
+            const parser = new JQueryParser();
+            const result = parser.parse(allElements);
+            const test = result.map((r) => r.listeners.length).reduce((a, b) => a + b, 0);
+            expect(test).to.be.equal(2, 'parser did not return the correct number of events');
+            done();
+        }, 500);
+    });
+
+    it('reports the correct version', (done) => {
+        setTimeout(() => {
+            const parser = new JQueryParser();
+            const result = parser.parse(allElements);
+            expect(parser.name()).to.equal('jQuery1.9.11.9.1', 'Incorrect version reported');
+            done();
+        }, 500);
+    });
+});
